6c0245623fc5653d5247ddc01b91110ae49eb260
Eskil Nysether <47796979+Esknys@users.noreply.github.com>
2020-05-08 16:23:02 +0200

    Klasser for binærfilbehandling

---
+++ src/main/java/com/sample/binaryfile/GearboxFormatter.java
@@ -0,0 +1,16 @@
+package com.sample.binaryfile;
+
+import com.sample.car.Gearbox;
+
+public class GearboxFormatter {
+
+    public static String DELIMITER = "::";
+
+    public static String formatGearbox(Gearbox g) {
+
+        String name = g.getName();
+        String price = Integer.toString(g.getPrice());
+        String
+
+    }
+}
