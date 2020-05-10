3c692a808ba0bf36c5a55fce753553696a824e8a
Eskil Nysether <47796979+Esknys@users.noreply.github.com>
2020-05-10 19:00:38 +0200

    Binærfilbehandling

--- src/main/java/com/sample/textfile/FileReader.java
+++ src/main/java/com/sample/textfile/FileReader.java
@@ -1,9 +1,6 @@
 package com.sample.textfile;
 
-import com.sample.car.Car;
-import com.sample.car.Engine;
-import com.sample.car.Gearbox;
-import com.sample.car.Paintjob;
+import com.sample.car.*;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
 
@@ -39,7 +36,7 @@ public class FileReader {
     private static Car parseCar(String line)
             throws InvalidCarFormatException {
         String[] split = line.split(CarFormatter.DELIMITER);
-        if(split.length != 11) {
+        if(split.length != 15) {
             throw new InvalidCarFormatException("Must use two colons to separate the five data fields");
         }
 
@@ -54,17 +51,25 @@ public class FileReader {
         String color = split[8];
         String type = split[9];
         String paintjobPrice = split[10];
+        String wheelName = split[11];
+        String wheelType = split[12];
+        String wheelSize = split[13];
+        String wheelPrice = split[14];
 
         Integer paintjobPrice1 = Integer.valueOf(paintjobPrice);
         Integer enginePrice1 = Integer.valueOf(enginePrice);
         Integer gearboxPrice1 = Integer.valueOf(gearboxPrice);
         Integer horsepower1 = Integer.valueOf(horsepower);
+        Integer wheelSize1 = Integer.valueOf(wheelSize);
+        Integer wheelPrice1 = Integer.valueOf(wheelPrice);
 
         Engine engine = new Engine(fuelName, fuel, horsepower1, enginePrice1);
         Gearbox gearbox = new Gearbox(gearboxName, gearboxPrice1, gearboxType);
         Paintjob paintjob = new Paintjob(paintjobName, paintjobPrice1, color, type);
+        Wheel wheel = new Wheel(wheelName, wheelType, wheelSize1, wheelPrice1);
 
-        return new Car(engine, gearbox, paintjob);
+        return new Car(engine, gearbox, paintjob, wheel);
     }
+
 
 }
